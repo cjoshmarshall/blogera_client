f1de2cb86822613bdb26d36923094791ac04d6cf
cjoshmarshall <cjoshmarshall@gmail.com>
2022-04-29 11:13:30 +0530

    fix 1.1

--- src/api/apiHandle.js
+++ src/api/apiHandle.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // export const BASE_URL="http://localhost:3006/api"
-export const BASE_URL="https://blogera.herokuapp.com/api"
+export const BASE_URL="https://blogeraa.herokuapp.com/api"
 
 export const publicRequest=axios.create({
     baseURL:BASE_URL
